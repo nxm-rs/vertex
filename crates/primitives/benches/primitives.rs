987de250b69406a86bba40aba33fa4ffeaa61354
mfw78 <mfw78@nullis.xyz>
2025-01-16 12:29:17 +0000

    chore(bmt): use constant variable

--- crates/primitives/benches/primitives.rs
+++ crates/primitives/benches/primitives.rs
@@ -1,13 +1,13 @@
 #![allow(unknown_lints, clippy::incompatible_msrv, missing_docs)]
 #![feature(async_closure)]
 
+use alloy_primitives::keccak256;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use rand::prelude::*;
-//use swarm_primitives::bmt::chunk::{Chunk, Options};
-use alloy_primitives::keccak256;
 use swarm_primitives::bmt::reference::RefHasher;
 use swarm_primitives::bmt::{Hasher, HasherBuilder};
 use swarm_primitives::Address;
+use swarm_primitives::BMT_BRANCHES;
 use swarm_primitives::{distance, proximity};
 use tokio::runtime::Builder;
 
@@ -28,14 +28,14 @@ pub fn primitives(c: &mut Criterion) {
         })
     });
     g.bench_function("bmt_nonconcurrent", |b| {
-        let hasher = RefHasher::<128>::new();
+        let hasher: RefHasher<BMT_BRANCHES> = RefHasher::new();
         b.iter(|| {
             black_box(hasher.hash(&random_chunk));
         })
     });
     g.bench_function("bmt_concurrent", |b| {
         let rt = Builder::new_multi_thread()
-            .worker_threads(16)
+            //.worker_threads(4)
             .build()
             .unwrap();
         b.to_async(&rt).iter(|| async {
